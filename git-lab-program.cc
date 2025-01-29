09dbf0bffeea554db7bf3118591eb10ac94f55c3
sweenejj <js095423@ohio.edu>
2025-01-29 12:42:05 -0500

    Step 15

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Joshua Sweeney>
+ *        Date: <1/29/25>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
